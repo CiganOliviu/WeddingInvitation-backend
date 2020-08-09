26f26e209b35a728ef7327964114a2a7915bb8ba
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-08-09 21:50:36 +0300

    Update backend

--- WeddingInvitation/MainView/admin.py
+++ WeddingInvitation/MainView/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ConfirmAnswer
 
-# Register your models here.
+class ConfirmAnswerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'answer_sent')
+
+admin.site.register(ConfirmAnswer, ConfirmAnswerAdmin)
--- WeddingInvitation/MainView/models.py
+++ WeddingInvitation/MainView/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ConfirmAnswer (models.Model):
+
+    name = models.CharField(max_length=200, unique=True)
+    submitted = models.BooleanField(default = True)
+    answer_sent = models.DateTimeField(auto_now_add=True)
